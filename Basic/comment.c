b7585565dc238eccc05657f52624f1f3dcfee1db
bardhanshrestha <bardhanshrestha@gmail.com>
2026-09-13 02:30:08 +0600

    Add files via upload

---
+++ Basic/comment.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+printf("Hello World!\n");
+printf("I am learning C.");
+  //  Print "Hello World!" with a new line
+  //  Print "I am learning C."
+/*Print "Hello World!" with a new line ,Print "I am learning C.*/
+  return 0;
+}
---
+++ Basic/sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a;
+    int b;
+    int sum;
+    printf("enter a"); 
+    scanf("%d",&a); 
+    printf("enter b");
+    scanf("%d",&b);
+    sum = a + b;
+    printf("sum is %d",sum);
+    return 0;
+
+}
